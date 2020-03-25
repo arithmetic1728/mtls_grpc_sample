3c89f96cc92ba026dc32f825e2bf94677d5e71e6
Sijun Liu <sijunliu@google.com>
2020-03-25 12:04:21 -0700

    update lib

--- http_sample.py
+++ http_sample.py
@@ -13,35 +13,35 @@ credentials, project_id = google.auth.default()
 credentials = google.auth.credentials.with_scopes_if_required(
     credentials, ["https://www.googleapis.com/auth/pubsub"]
 )
-print("======= project_id ========")
+print("project_id is: ")
 print(project_id)
-print("======= credentials ========")
-print(credentials)
 
 
 def use_requests():
+    print("====== Test http call with requests lib =====\n\n\n")
     authed_session = google.auth.transport.requests.AuthorizedSession(credentials)
     authed_session.configure_mtls_channel()
 
     if authed_session.is_mtls:
-        print("====== use mtls channel=====")
+        print("= using mtls channel=")
         response = authed_session.get(MTLS_ENDPOINT.format(project_id))
     else:
-        print("====== use regular channel=====")
+        print("= using regular channel=")
         response = authed_session.get(REGULAR_ENDPOINT.format(project_id))
 
     print(response.content)
 
 
 def use_urllib3():
+    print("====== Test http call with urllib3 lib =====\n\n\n")
     authed_http = google.auth.transport.urllib3.AuthorizedHttp(credentials)
     is_mtls = authed_http.configure_mtls_channel()
 
     if is_mtls:
-        print("====== use mtls channel=====")
+        print("= using mtls channel=")
         response = authed_http.request("GET", MTLS_ENDPOINT.format(project_id))
     else:
-        print("====== use regular channel=====")
+        print("= using regular channel=")
         response = authed_http.request("GET", REGULAR_ENDPOINT.format(project_id))
 
     print(response.data)
